88dd844a0507bf2d3d3b7ce6983d37ebf0f16941
Thamodh Egodawatte <thamodhegodawatte@Thamodhs-MacBook-Pro.local>
2019-03-16 16:56:35 -0400

    Add getters and setters to Player class
    A Player Class contains information about the player's number and
    the colour of the disks the player is using. These attributes will
    be used in other functions in the model.

---
+++ src/player.py
@@ -0,0 +1,31 @@
+import c4_model
+from point import Point
+from disk import Disk
+
+"""
+A Player class contains the information about the player. The information
+about the player include the player's number and the colour of disk.
+"""
+
+class Player:
+
+    def __init__(self, number, color_disk):
+        self.number = number
+        self.colorDisk = color_disk
+        self.numDisks = num_disks 
+    
+    def get_player_number(self):
+        return self.number
+    
+    def set_player_number(self, number):
+        self.number = number
+        
+    def get_color_disk(self):
+        return self.color_disk
+    
+    def set_color_disk(self, color_disk):
+        self.color_disk = color_disk
+        
+ 
+    
+    
