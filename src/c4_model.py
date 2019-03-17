7c5bbbc1f54ecdbfc67c6af78429920869500c6a
kevinsub <kevinsubhash@gmail.com>
2019-03-17 11:34:01 -0400

    Add game_over function
    This function from the c4_model.py file checks if a player won
    the game. It reads though the game board and tries to find if
    four of the player's disks are in a row. It returns true if the
    player won and false if the player didn't.

--- src/c4_model.py
+++ src/c4_model.py
@@ -22,12 +22,32 @@ class model:
         for i in range(7):
             this.frame.append([0, 0, 0, 0, 0, 0, 0])
     
-    def game_over(self):
+    def game_over(self, player):
         """
-        returns True if the game is one by a player.
-        Should work based on the spacing of disks?
+        returns true when a player wins the game.
+        This function checks if the player placed four 
+        chips vertically, horizontally, or diagonally.
         """
-        pass
+        for height_idx in range (5,-1,-1):
+            for position in range(4):
+                if [player.get_player_number()]*4 == self.frame[height_idx][position:position+4]:
+                    return True
+        for length_idx in range (0,7,1):
+            for position in range (4):
+                if (player.get_player_number() == self.frame[position][length_idx] == self.frame[position+1][length_idx] == 
+                    self.frame[position+2][length_idx] == self.frame[position+3][length_idx]):
+                    return True
+        for position in range (3):
+            for height_idx in range(0,4,1):
+                if (player.get_player_number() == self.frame[position][height_idx] == self.frame[position+1][height_idx+1] == 
+                    self.frame[position+2][height_idx+2] == self.frame[position+3][height_idx+3]):
+                    return True
+        for position in range (3):
+            for length_idx in range(3,7,1):
+                if (player.get_player_number() == self.frame[position][length_idx] == self.frame[position+1][length_idx-1] == 
+                    self.frame[position+2][length_idx-2] == self.frame[position+3][length_idx-3]):
+                    return True
+        return False        
     
     def insert_disk(self, player, column):
         """
