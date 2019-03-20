21955a3495f2c520605cd0cbb0dafe022313553e
kevinsub <kevinsubhash@gmail.com>
2019-03-20 18:11:10 -0400

    Add documentation to c4model.py
    Documentation has been added to the model class. Each function
    now has a proper description of what it does.

--- src/c4model.py
+++ src/c4model.py
@@ -1,19 +1,27 @@
 import pygame
-#import disk
 from player import Player
 from point import Point
+from disk import Disk
 
 """
-This will quite literally be the board within the connect 4 game.
-The model houses all the disks.
+This is a model class. This class is used to update and keep track of 
+the connect four game board. It contains several functions which are 
+used to check if the game is over or when a player is inserting a disk.
+
+Functions:
+
+def game_over(self, disk):
+def insert_disk(self, column):
+def is_filled(self):
+def turn():
+
 
 """
 class Model:
     
     def __init__(self):
         """
-        intialized the framework of the game, setting 0 as a space in the frame
-        without a disk
+        An init function that is used to create and update the game board object.
         """
         #self.commands = c4_commands(); #added a command queue
         self.current_player = 1
@@ -24,15 +32,20 @@ class Model:
     
     def game_over(self, disk):
         """
-        returns true when a player wins the game.
-        This function checks if the player placed four 
-        chips vertically, horizontally, or diagonally.
+        This function checks if the disk that is inserted caused 
+        a connection of four disks. If so, this results in a win 
+        for the player. It checks if there is a row of four 
+        horizontally, vertically, or diagonally. It returns true 
+        if there is a connection of four indicating that the game 
+        is over. It returns false if there is no connection of four 
+        disks.
+        
         """
-        x_coord = disk.getPoint().getX()
+        x_coord = disk.getPoint().getX() # Getting x and y coordinates of the passed in disk object
         y_coord = disk.getPoint().getY()
         consecutive_disks = [0,0,0,0,0,0,0,0]
         
-        for shift in range (1, 4, 1):
+        for shift in range (1, 4, 1): # Checking if there is a connection of four disks horizonally, vertically, or diagonally
             if(self.frame[x_coord-shift][y_coord] == disk):
                 consecutive_disks[0] += 1
             if(self.frame[x_coord+shift][y_coord] == disk):
@@ -50,29 +63,41 @@ class Model:
             if(self.frame[x_coord-shift][y_coord-shift] == disk):
                 consecutive_disks[7] += 1
                 
-        if(3 in consecutive_disks):
+        if(3 in consecutive_disks): # Returns true if there four disks are connected
             return True
         return False        
     
     def insert_disk(self, column):
         """
-        inserts a player's disk on the frame and updates 
-        it. It will require the player number and the column 
-        the player chooses to place his/her disk.
+        This function inserts a disk in the column that the 
+        player has chosen. It checks if there is space in 
+        the specified column and then creates a disk object in 
+        that position. If there is no space in the column, it 
+        will return false. If it inserts a disk object successfully, 
+        it returns true.
         """
-        if (is_filled() == False):
-            for row in range (5,-1,-1):
+        if (is_filled() == False): # Checks if board is not full
+            for row in range (5,-1,-1): # Loops through rows to make sure there is an empty space to create a disk object
                 if self.frame[row][column] == None:
                     self.frame[row][column] = Disk(point(row, column), self.current_player)
                     self.available_slots -= 1
-                    turn()
+                    turn() # Changes turn to next player once disk is 'inserted'
                     return True
         return False 
     
     def is_filled(self):
-        if self.available_slots == 0:
+        """
+        This function checks if the board is full. The total available 
+        slots is 42 and once it reaches 0, the board is full. It returns 
+        true if the available slots is 0 and returns false otherwise.
+        """
+        if self.available_slots == 0: # Checks if available_slots variable is 0
             return True
         return False
     
     def turn():
+        """
+        This function switches the turn of the two players 
+        playing the game.
+        """
         self.current_player = 3 - current_player    
